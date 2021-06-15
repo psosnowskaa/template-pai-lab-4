1740a74dd18c69672c97bf2bb65e39ed699dccb9
Patrycja <patrycja.sosnowska@student.wat.edu.pl>
2021-06-15 13:33:36 +0200

    Zadania 4.3 - logowanie i wylogowanie.

--- support/query.sql
+++ support/query.sql
@@ -71,4 +71,12 @@ ALTER TABLE KsiazkiZamowienia
     ADD CONSTRAINT `KsiazkiZamowieniaKsiazkaIdFK` FOREIGN KEY (`KsiazkaId`) REFERENCES Ksiazki(`Id`);
 
 -- Dodanie początkowej zawartości do tabel.
+ALTER TABLE Klienci ADD COLUMN `Email` VARCHAR(245) UNIQUE;
+ALTER TABLE Klienci ADD COLUMN `Haslo` VARCHAR(100) NOT NULL;
+
+ALTER TABLE Klienci CHANGE COLUMN `Nr domu` `Nr_domu` VARCHAR(5) NOT NULL;
+ALTER TABLE Klienci CHANGE COLUMN `Nr mieszkania` `Nr_mieszkania` VARCHAR(5);
+
+ALTER TABLE Klienci CHANGE COLUMN `Miasto` `Miejscowosc` VARCHAR(60) NOT NULL;
+
 
